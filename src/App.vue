d391d68ebb8b9e4066da7af6abbb764cce1598d8
Joao Souza <joao.souza@avenuecode.com>
2023-04-30 16:45:24 -0300

    finishing routes transitions

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <nav-bar />
   <section id="view-container">
-    <router-view />
+    <router-view v-slot="{ Component }">
+      <transition name="slide" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </router-view>
   </section>
 </template>
 
 <script setup>
-import NavBar from '@/components/Layout/NavBar.vue'
+import NavBar from '@/components/Layout/NavBar.vue';
 </script>
 
 <style lang="less">
@@ -19,5 +23,14 @@ import NavBar from '@/components/Layout/NavBar.vue'
   max-width: 80%;
   margin: 0 auto;
   min-height: 92.5svh;
+
+  .slide-leave-active {
+    transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+    opacity: 0;
+  }
+
+  .slide-leave-to {
+    transform: translateX(100%);
+  }
 }
 </style>
