44b4c491a9f2bfb6849093a2437989a0a7107280
Joao Souza <joao.souza@avenuecode.com>
2023-06-11 14:04:18 -0300

    Adding documents

--- src/service/utils/firebase.js
+++ src/service/utils/firebase.js
@@ -1,16 +1,21 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+import { getStorage } from "firebase/storage";
+
 
 const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: 'd'
+  apiKey: 'AIzaSyCOzXpbyUlmLhtDTazMZ_jQtP52DuBNlVs',
+  authDomain: 'libras-project-786d5.firebaseapp.com',
+  projectId: 'libras-project-786d5',
+  storageBucket: 'libras-project-786d5.appspot.com',
+  messagingSenderId: '255427123080',
+  appId: '1:255427123080:web:7c9c6d7e13f02cd8390c3d'
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
+
